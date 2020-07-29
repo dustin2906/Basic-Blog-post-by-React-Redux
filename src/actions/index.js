04b247c7b910fa2a05041bc149cd0881bafa862b
= <=>
2020-07-29 14:53:12 +0300

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+import _, { values } from 'lodash';
+import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+
+export const fetchPostAndUser = () => async (dispatch,getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
+};
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceHolder.get('posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data})
+  };
+
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceHolder.get(`/users/${id}`)
+
+  dispatch({ type: 'FETCH_USER', payload: response.data});
+};
